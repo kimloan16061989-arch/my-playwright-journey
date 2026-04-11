1476dce8549b88340cab401a7a50c90eeb3e6772
loan <kimloan16061989@gmail.com>
2026-04-11 20:35:56 +0700

    test: Add comment to practice file

---
+++ tests/login.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Day 02 - Git practice', async ({ page }) => {
+  await page.goto('https://google.com');
+  await expect(page).toHaveTitle(/Google/);
+  console.log('✅ Day 02 done!');
+})
+// Đây là bài tập Day 02 - Git Basics
+import { test, expect } from '@playwright/test';
